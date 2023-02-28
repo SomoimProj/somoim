ef70a78fe7489e0fb92d1e8616bf6e17ba2becdf
yl0937 <yl1033371@gmail.com>
2023-02-28 09:40:57 +0900

    Initial Commit

---
+++ somoim/src/test/java/com/Oinzo/somoim/SomoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oinzo.somoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomoimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
