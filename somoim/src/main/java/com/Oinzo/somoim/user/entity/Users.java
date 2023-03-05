a3296fdfd1b16ed884e61d0e64623506a8a11b90
Jin YongKyung <jinga5000@naver.com>
2023-03-05 18:03:15 +0900

    feat: Users 엔티티 작성

--- somoim/src/main/java/com/Oinzo/somoim/user/entity/Users.java
+++ somoim/src/main/java/com/Oinzo/somoim/user/entity/Users.java
@@ -2,8 +2,37 @@ package com.Oinzo.somoim.user.entity;
 
 import com.Oinzo.somoim.common.entity.BaseEntity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class Users extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String birth;
+
+    @Column
+    private String gender;
+
+    @Column
+    private String area;
+
+    @Column
+    private String introduction;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String profileUrl;
+
+    @Column
+    private String favorite;
 }
