22b13445e9c3ab76d853dfa7d58f35b78b0d01f7
Jin YongKyung <jinga5000@naver.com>
2023-03-06 16:43:18 +0900

    chore : user 패키지 내 repository 에 JpaRepository 상속

--- somoim/src/main/java/com/Oinzo/somoim/user/repository/LikeRepository.java
+++ somoim/src/main/java/com/Oinzo/somoim/user/repository/LikeRepository.java
@@ -1,7 +1,9 @@
 package com.Oinzo.somoim.user.repository;
 
+import com.Oinzo.somoim.user.entity.Likes;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LikeRepository {
+public interface LikeRepository extends JpaRepository<Likes, Long> {
 }
