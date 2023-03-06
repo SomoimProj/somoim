3ae9688cf5fd7b5a2b66fc130ed1177cfd17877f
Jin YongKyung <jinga5000@naver.com>
2023-03-07 04:00:47 +0900

    chore : @RequestParam으로 받는 전화번호로 인증번호 발송 가능

--- somoim/src/main/java/com/Oinzo/somoim/user/controller/MessageAuthController.java
+++ somoim/src/main/java/com/Oinzo/somoim/user/controller/MessageAuthController.java
@@ -2,7 +2,6 @@ package com.Oinzo.somoim.user.controller;
 
 import com.Oinzo.somoim.user.service.SmsAuthenticationService;
 import lombok.RequiredArgsConstructor;
-import net.nurigo.java_sdk.exceptions.CoolsmsException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +16,11 @@ public class MessageAuthController {
     private final SmsAuthenticationService messageService;
 
     @PostMapping("/send-one")
-    public ResponseEntity<?> sendSms(@RequestParam(value = "to") String to) throws CoolsmsException {
+    public ResponseEntity<?> sendSms(@RequestParam(value = "to") String to) {
 
         String sms = messageService.sendSms(to);
 
         return ResponseEntity.ok(sms);
     }
+
 }
