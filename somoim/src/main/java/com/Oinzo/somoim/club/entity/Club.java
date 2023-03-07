3e2b306b99a92c74edb87356f5b773aeeea1cb9a
yl0937 <yl1033371@gmail.com>
2023-03-07 23:50:06 +0900

    feat: feat: Club 엔티티 작성 (#7)
    * feat: Club 엔티티 작성

--- somoim/src/main/java/com/Oinzo/somoim/club/entity/Club.java
+++ somoim/src/main/java/com/Oinzo/somoim/club/entity/Club.java
@@ -1,9 +1,39 @@
 package com.Oinzo.somoim.club.entity;
 
 import com.Oinzo.somoim.common.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Club extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String area;
+
+    @Column(nullable = false)
+    private int memberLimit;
+
+    @Column(nullable = false)
+    private int memberCnt;
+
+    @Column(nullable = false)
+    private String favorite;
 }
