18cb6a7d50443fb3fb8594b235e5e650f590c2d8
Jin YongKyung <jinga5000@naver.com>
2023-03-10 16:42:20 +0900

    refactor : Users -> User

--- somoim/src/main/java/com/Oinzo/somoim/user/entity/User.java
+++ somoim/src/main/java/com/Oinzo/somoim/user/entity/User.java
@@ -2,16 +2,18 @@ package com.Oinzo.somoim.user.entity;
 
 import com.Oinzo.somoim.common.entity.BaseEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Users extends BaseEntity {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,15 +31,26 @@ public class Users extends BaseEntity {
     @Column(nullable = false)
     private String area;
 
-    @Column
-    private String introduction;
-
     @Column(nullable = false)
     private String phoneNumber;
+
+    @Column
+    private String introduction;
 
     @Column
     private String profileUrl;
 
     @Column
     private String favorite;
+
+    public User(String name, String birth, String gender, String area) {
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+        this.area = area;
+    }
+
+    public User(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
