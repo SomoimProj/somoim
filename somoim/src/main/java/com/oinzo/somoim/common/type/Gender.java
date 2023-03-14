49d830e47b7f5be25696af60930b0e582d02da6e
soeunkk <luckyber1@naver.com>
2023-03-14 12:59:50 +0900

    fix: H2 DB 연동 제거 및 Users 대신 User로 변경

---
+++ somoim/src/main/java/com/oinzo/somoim/common/type/Gender.java
@@ -0,0 +1,6 @@
+package com.oinzo.somoim.common.type;
+
+public enum Gender {
+	MALE,
+	FEMALE
+}
--- somoim/src/main/java/com/oinzo/somoim/user/entity/User.java
+++ somoim/src/main/java/com/oinzo/somoim/user/entity/User.java
@@ -1,43 +1,32 @@
 package com.oinzo.somoim.user.entity;
 
 import com.oinzo.somoim.common.entity.BaseEntity;
+import com.oinzo.somoim.common.type.Gender;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
-public class Users extends BaseEntity {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private String birth;
-
-    @Column(nullable = false)
-    private String gender;
-
-    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
     private String area;
-
-    @Column
     private String introduction;
-
-    @Column(nullable = false)
-    private String phoneNumber;
-
-    @Column
     private String profileUrl;
-
-    @Column
     private String favorite;
 }
